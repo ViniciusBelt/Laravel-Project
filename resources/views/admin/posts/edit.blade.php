63a30bae6e355f273775861bf95d4accd18dabe7
ViniciusBelt <super.vi@hotmail.com>
2022-06-15 10:37:23 -0300

    Correção para desabilitar campos

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -3,19 +3,20 @@
     @csrf
     <div class="form-group">
         <label for="title" style="float: left;">Tipo de Solicitação</label>
-        <input type="text" class="form-control" id="tipo_solicitacao" name="tipo_solicitacao" value="{{ $event->tipo_solicitacao }}" maxlength="50" @if($event->id == 3 || $event->id == 4 || $event->id == 5) disabled @endif>
+        <input type="text" class="form-control" id="tipo_solicitacao" name="tipo_solicitacao" value="{{ $event->tipo_solicitacao }}" maxlength="50" @if($event->id_etapa == 3 || $event->id_etapa == 4 || $event->id_etapa == 5) disabled @endif>
     </div>
     <div class="form-group">
         <label for="title" style="float: left;">Cliente</label>
-        <input type="text" class="form-control" id="cliente" name="cliente" value="{{ $event->cliente }}" maxlength="50" @if($event->id == 3 || $event->id == 4 || $event->id == 5) disabled @endif>
+        <input type="text" class="form-control" id="cliente" name="cliente" value="{{ $event->cliente }}" maxlength="50" @if($event->id_etapa == 3 || $event->id_etapa == 4 || $event->id_etapa == 5) disabled @endif>
     </div>
     <div class="form-group">
         <label for="title" style="float: left;">Solicitante</label>
-        <input type="text" class="form-control" id="solicitante" name="solicitante" value="{{ $event->solicitante }}" maxlength="50" @if($event->id == 3 || $event->id == 4 || $event->id == 5) disabled @endif>
+        <input type="text" class="form-control" id="solicitante" name="solicitante" value="{{ $event->solicitante }}" maxlength="50" @if($event->id_etapa == 3 || $event->id_etapa == 4 || $event->id_etapa == 5) disabled @endif>
     </div>
     <div class="form-group">
         <label for="title" style="float: left;">Etapa</label><br>
         <select name="id_etapa" id="id_etapa" class="form-control">
+            <option value="{{$event->id_etapa}}" selected disabled>Etapa atual: {{$event->etapa->descricao}}</option>
             @foreach($etapas as $etapa)
             <option value="{{$etapa->id}}">{{$etapa->descricao}}</option>
             @endforeach
@@ -23,11 +24,11 @@
     </div>
     <div class="form-group">
         <label for="title" style="float: left;">CPF/CNPJ/ID</label>
-        <input type="text" class="form-control" id="cpf_cnpj_id" name="cpf_cnpj_id" value="{{ $event->cpf_cnpj_id }}" maxlength="14" @if($event->id == 3 || $event->id == 4 || $event->id == 5) disabled @endif>
+        <input type="text" class="form-control" id="cpf_cnpj_id" name="cpf_cnpj_id" value="{{ $event->cpf_cnpj_id }}" maxlength="14" @if($event->id_etapa == 3 || $event->id_etapa == 4 || $event->id_etapa == 5) disabled @endif>
     </div>
     <div class="form-group">
         <label for="title" style="float: left;">Data de Vencimento</label>
-        <input type="date" class="form-control" id="data_aprovacao" name="data_aprovacao" value="{{ $event->data_aprovacao }}" @if($event->id == 3 || $event->id == 4 || $event->id == 5) disabled @endif>
+        <input type="date" class="form-control" id="data_aprovacao" name="data_aprovacao" value="{{ $event->data_aprovacao }}" @if($event->id_etapa == 3 || $event->id_etapa == 4 || $event->id_etapa == 5) disabled @endif>
     </div>
     <?php
         $data = date('Y-m-d'); 
