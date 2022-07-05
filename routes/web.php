9dab60dc51506ee700d7053ce89500abfe992f84
ViniciusBelt <super.vi@hotmail.com>
2022-07-05 10:17:42 -0300

    Implementando middleware

--- routes/web.php
+++ routes/web.php
@@ -6,42 +6,31 @@ use App\Http\Controllers\{
 };
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PostController::class, 'index'])->name('index');
+
+Route::get('/loginFunc', 'App\Http\Controllers\Authenticate@login')->name('login');
+Route::get('/logout', 'App\Http\Controllers\Authenticate@logout')->name('logout');
 
 Route::get('/registrar', function () {
     return view('registrar');
 })->name('registrar');
-
 Route::get('/registrando', 'App\Http\Controllers\Authenticate@criarUsuario')->name('registrando');
 
 Route::get('/login', function () {
     return view('login');
 })->name('pgLogin');
 
-Route::get('/objetivo', [PostController::class, 'objetivo'])->name('objetivo');
-
-Route::get('/editObjetivo', [PostController::class, 'editObjetivo'])->name('editObjetivo');
-
-Route::post('/updateObjetivo', [PostController::class, 'updateObjetivo']);
-
-Route::get('/loginFunc', 'App\Http\Controllers\Authenticate@login')->name('login');
-
-Route::get('/logout', 'App\Http\Controllers\Authenticate@logout')->name('logout');
-
-Route::post('/', [PostController::class, 'salvar']);
-
-Route::delete('/{id}', [PostController::class, 'destroy']);
-
-Route::get('/edit/{id}', [PostController::class, 'edit']);
-
-Route::post('/update/{id}', [PostController::class, 'update']);
-
-Route::get('/solicitacoes', [PostController::class, 'solicitacoes'])->name('solicitacoes');
-
-Route::get('/acessos', [PostController::class, 'acessos'])->name('acessos');
-
-Route::delete('/excluirUser/{id}', [PostController::class, 'destroyUser']);
-
-Route::get('/editUser/{id}', [PostController::class, 'editUser']);
-
-Route::post('/updateUser/{id}', [PostController::class, 'updateUser']);
+Route::middleware(['auth'])->group(function (){
+    Route::get('/', [PostController::class, 'index'])->name('index');
+    Route::get('/objetivo', [PostController::class, 'objetivo'])->name('objetivo');
+    Route::get('/editObjetivo', [PostController::class, 'editObjetivo'])->name('editObjetivo');
+    Route::post('/updateObjetivo', [PostController::class, 'updateObjetivo']);
+    Route::post('/', [PostController::class, 'salvar']);
+    Route::delete('/{id}', [PostController::class, 'destroy']);
+    Route::get('/edit/{id}', [PostController::class, 'edit']);
+    Route::post('/update/{id}', [PostController::class, 'update']);
+    Route::get('/solicitacoes', [PostController::class, 'solicitacoes'])->name('solicitacoes');
+    Route::get('/acessos', [PostController::class, 'acessos'])->name('acessos');
+    Route::delete('/excluirUser/{id}', [PostController::class, 'destroyUser']);
+    Route::get('/editUser/{id}', [PostController::class, 'editUser']);
+    Route::post('/updateUser/{id}', [PostController::class, 'updateUser']);
+});
